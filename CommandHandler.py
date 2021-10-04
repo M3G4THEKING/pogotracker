7af68c805ef18ff1e8557b907eaec160131127e7
kiviel <spadadellaregina@gmail.com>
2021-10-04 16:04:44 +0200

    completato

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,23 +1,32 @@
 
 from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler, Filters, InlineQueryHandler, MessageHandler
 
+from Admin import admin, ban, eliminapalestra, listaraid, palestra, unban, verifica
+from Functions import codice_amico, gym, info, livello, nickname, raid, readChat, start, team
 from InlineButton import InlineRaid
-from Functions import codice_amico, getUser, info, livello, nickname, raid, start, team
 
 def loadCallbackQueries(dispatcher: Dispatcher) -> None:
 	dispatcher.add_handler(CallbackQueryHandler())
 
 def loadCommands(dispatcher: Dispatcher) -> None:
+	dispatcher.add_handler(CommandHandler("admin", admin))
+	dispatcher.add_handler(CommandHandler("ban", ban))
 	dispatcher.add_handler(CommandHandler("codice_amico", codice_amico))
+	dispatcher.add_handler(CommandHandler("eliminapalestra", eliminapalestra))
+	dispatcher.add_handler(CommandHandler("gym", gym))
 	dispatcher.add_handler(CommandHandler("info", info))
+	dispatcher.add_handler(CommandHandler("listaraid", listaraid))
 	dispatcher.add_handler(CommandHandler("livello", livello))
 	dispatcher.add_handler(CommandHandler("nickname", nickname))
+	dispatcher.add_handler(CommandHandler("palestra", palestra))
 	dispatcher.add_handler(CommandHandler("raid", raid))
 	dispatcher.add_handler(CommandHandler("start", start))
 	dispatcher.add_handler(CommandHandler("team", team))
+	dispatcher.add_handler(CommandHandler("unban", unban))
+	dispatcher.add_handler(CommandHandler("verifica", verifica))
 
 def loadInlineQueries(dispatcher: Dispatcher) -> None:
 	dispatcher.add_handler(InlineQueryHandler(InlineRaid))
 
 def loadMessageHandler(dispatcher: Dispatcher) -> None:
-	dispatcher.add_handler(MessageHandler(Filters.all, getUser))
+	dispatcher.add_handler(MessageHandler(Filters.all, readChat))
