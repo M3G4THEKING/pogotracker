6a081c88316be19c8451a0549a033ace3bd982b5
kiviel <spadadellaregina@gmail.com>
2021-10-05 09:44:18 +0200

    fix permlevel

--- Auth.py
+++ Auth.py
@@ -3,10 +3,10 @@ from JSONParser import getConfig
 
 def denyCommand(update):
 	config = getConfig()
-	if permLevel(update.message.from_user.id) > 0 and (update.message.chat_id == config["admin"] or update.message.chat.type == "private"):
+	if permLevel(update.message.from_user.id) > 0 and (update.message.chat_id == config["chatadmin"] or update.message.chat.type == "private"):
 		return False
 	return True
 
 def permLevel(IDUtente: int):
-	utente = getAuth(IDUtente)
-	return utente.PermLevel if utente else 0
+	perm = getAuth(IDUtente)
+	return perm if perm else 0
